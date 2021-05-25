33369d9e2509366152519996564e3f78792d14ce
Olga Kovtun <83074160+olhakovtun@users.noreply.github.com>
2021-05-25 18:05:44 +0200

    Lesson 3 (#1)

--- src/test/java/AscendingSequenceTest.java
+++ src/test/java/AscendingSequenceTest.java
@@ -59,5 +59,100 @@ public class AscendingSequenceTest {
         Assert.assertArrayEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void testAscendingSequenceStringHappyPath() {
+        int startNumber = 1;
+        int endNumber = 5;
+        String expectedResult = "1, 2, 3, 4, 5";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringHappyPathZero() {
+        int startNumber = 0;
+        int endtNumber = 5;
+        String expectedResult = "0, 1, 2, 3, 4, 5";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringHappyPathZeroToTen() {
+        int startNumber = 0;
+        int endtNumber = 10;
+        String expectedResult = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringHappyPathNegativeFiveToTen() {
+        int startNumber = -5;
+        int endtNumber = 10;
+        String expectedResult = "-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringHappyPathNegativeFiveToZero() {
+        int startNumber = -5;
+        int endtNumber = 0;
+        String expectedResult = "-5, -4, -3, -2, -1, 0";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringNegativeTest() {
+        int startNumber = 5;
+        int endtNumber = 0;
+        String expectedResult = "Error";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringNegativeStartNumberEqualEndNumber() {
+        int startNumber = -9;
+        int endtNumber = -9;
+        String expectedResult = "Error";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequenceStringNegativeBigNumbers() {
+        int startNumber = -2147483648 - 1;
+        int endtNumber = 2147483647 + 1;
+        String expectedResult = "Error";
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        String actualResult = ascendingSequence.buildAscendingString(startNumber, endtNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 
 }
